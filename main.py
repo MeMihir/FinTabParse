ccb22ef34eaf448bb23709ef7e590168935053f8
Mihir <pavuskarmihir@gmail.com>
2022-04-17 18:00:52 +0530

    add question classifier

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def get_inputs():
 
 	return questions, paragraphs, table
 
-def load_classifier_model():
+def question_classifier_model():
 	tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-	labels = {'span':0, 'math':1}
-	classes = {0: 'span', 1: 'math'}
+	
 	question_classifier = BertClassifier()
 	question_classifier.load_state_dict(torch.load('/content/weights/questions_classifier', map_location=torch.device('cpu')))
+	
+	question_infs = list(map(lambda question_input: tokenizer(question_input, padding='max_length', max_length = 512, truncation=True, return_tensors="pt"), questions))
+	question_preds = []
+
+	for que_inf in question_infs:
+		question_id = que_inf['input_ids']
+		question_mask = que_inf['attention_mask'].squeeze(1)
+		pred = question_classifier(question_id, question_mask)
+		question_preds.append(pred.argmax(dim=1).numpy()[0])
