19cf1e540672f3ae09c5ee355697eec5dbffa98c
Mihir <pavuskarmihir@gmail.com>
2022-08-08 10:57:50 +0530

    update tapas aggregation

--- preprocessing/aggregation.py
+++ preprocessing/aggregation.py
@@ -2,39 +2,66 @@ import pandas as pd
 from utils.file_handling import dict_to_json, json_to_dict
 import numpy as np
 import copy
+from collections.abc import Iterable   # import directly from collections for Python < 3.3
+
+# Utility function to aggregate answers from multiple models
+
+def isAnsEmpty(ans):
+    if(type(ans)==type(1) or type(ans)==type(1.0)): return False
+    return len(ans)==0 or ans=="[]"
+
+
+def isFiltAnsEmpty(ans):
+    if(type(ans)==type(1) or type(ans)==type(1.0)): return False
+    try:
+        return len(ans)==0 or ans=="[]" or len(ans[0])==0
+    except TypeError:
+        return False
+
+# Aggregate answers from multiple models
 
 def aggregate_AlBERT(answers):
     answers = list(filter(lambda x: not isAnsEmpty(x["answer"]), answers))
     indx = max(answers, key=lambda x: x["score"])
     return indx
 
-def isAnsEmpty(ans):
-    if(type(ans)==type(1) or type(ans)==type(1.0)): return False
-    return len(ans)==0 or ans=="[]"
-
 def aggregate_TagOp(answers):
     ans = list(filter(lambda x: not isAnsEmpty(x["answer"][0]), answers))
     return ans
 
 def preprocess_tapas(answer):
-    if not ' > ' in answer:
-        return ["NONE", answer.split(', ')]
-    answer = answer.split(' > ')
-    aggr = answer[0]
-    answer = answer[1].split(', ')
-    answer = map(lambda x: x.replace(',', ''), answer)
-    answer = list(map(lambda x: float(x) if x.replace('.', '', 1).isdigit() else x, answer))
-    return [aggr, answer]
+    ans = answer['answer']
+    if not ' > ' in ans:
+        answer['answer'] = ["NONE", ans.split(', ')]
+    else:
+        ans = ans.split(' > ')
+        aggr = ans[0]
+        ans = ans[1].split(', ')
+        ans = map(lambda x: x.replace(',', ''), ans)
+        ans = list(map(lambda x: float(x) if x.replace('.', '', 1).isdigit() else x, ans))
+        answer['answer'] = [aggr, ans]
+    return answer
 
 
-def aggregate_TaPaS(answers):
-    answers = list(map(lambda x: {
-        "answer":  preprocess_tapas(x),
-        "score": 1.7390611171722412,
-        "chunk": 2,
-        "model": "TaPaS"
-    }, answers))
-    return answers
+def aggregate_tapas(ans_type, answers):
+  scores = []
+  agg_answers = []
+
+  if ans_type == 'NONE': 
+    agg_ans = list(filter(lambda x: x['answer'][0]=='NONE', answers))
+    if len(agg_ans)==0: return (0, [], 0)
+    
+    agg_ans = aggregate_AlBERT(agg_ans)
+    return (agg_ans['score'], agg_ans['answer'][1], len(answers))
+
+  for ans in answers:
+    if ans['answer'][0] != ans_type: continue
+    scores.append(ans['score'])
+    agg_answers.extend(ans['answer'][1])
+  agg_answers = list(filter(lambda x: not isinstance(x, Iterable) or len(x)!=0, agg_answers))
+  return (0, agg_answers, 0) if len(scores)==0 else (sum(scores)/len(scores), agg_answers, len(scores))
+
+# Main function to aggregate answers from multiple models
 
 def aggregate_answers(answers_path, op_path):
     answers = json_to_dict(answers_path)
@@ -54,24 +81,21 @@ def aggregate_answers(answers_path, op_path):
             if model == 'AlBERT': 
                 indx = aggregate_AlBERT(model_ans)
                 agg_ans[que]["answers"].append(indx)
-            elif model == "TaPaS":
-                indx = aggregate_AlBERT(model_ans)
-                agg_ans[que]["answers"].append(indx)
-            # elif model == "TagOp":
-            #     ans = aggregate_TagOp(model_ans)
-            #     agg_ans[que]["answers"].extend(ans)
+            elif model == "TaPaS": 
+                answers[que]['answers'] =  list(map(preprocess_tapas, answers[que]['answers']))
+                score_none, agg_none, len_none = aggregate_tapas('NONE', answers[que]['answers'])
+                score_avg, agg_avg, len_avg = aggregate_tapas('AVERAGE', answers[que]['answers'])
+                score_count, agg_count, len_count = aggregate_tapas('COUNT', answers[que]['answers'])
+                score_sum, agg_sum, len_sum = aggregate_tapas('SUM', answers[que]['answers'])
+                
+                if(len_none >= max([len_avg, len_count, len_sum])): agg_ans[que]['answers'] = {'answer': agg_none, 'score': score_none, 'model': 'TaPaS', 'aggregation': 'NONE'}
+                if(len_avg >= max([len_none, len_count, len_sum])): agg_ans[que]['answers'] = {'answer': agg_avg, 'score': score_avg, 'model': 'TaPaS', 'aggregation': 'AVG'}
+                if(len_count >= max([len_avg, len_none, len_sum])): agg_ans[que]['answers'] = {'answer': agg_count, 'score': score_count, 'model': 'TaPaS', 'aggregation': 'COUNT'}
+                if(len_sum >= max([len_avg, len_count, len_none])): agg_ans[que]['answers'] = {'answer': agg_sum, 'score': score_sum, 'model': 'TaPaS', 'aggregation': 'SUM'}
 
     dict_to_json(agg_ans, op_path)
 
     return agg_ans
-
-def isFiltAnsEmpty(ans):
-    if(type(ans)==type(1) or type(ans)==type(1.0)): return False
-    try:
-        return len(ans)==0 or ans=="[]" or len(ans[0])==0
-    except TypeError:
-        return False
-
         
 def filter_sort(input, filt, sort, output):
   answers = json_to_dict(input)
