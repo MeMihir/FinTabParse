a227e9f10c83d05f0ed2bd2ef3c148cc1a2b3af1
Mihir <pavuskarmihir@gmail.com>
2022-08-26 14:24:28 +0530

    add sentBERT cross-encoder

--- preprocessing/AlBERT_preprop.py
+++ preprocessing/AlBERT_preprop.py
@@ -1,6 +1,6 @@
 
 def prepare_AlQA_data(paragraphs):
-	return " ".join(paragraphs)
+	return ". ".join(paragraphs)
 
 
 def process_alqa_answers(preds, answers, chunk):
@@ -14,3 +14,12 @@ def process_alqa_answers(preds, answers, chunk):
     })
   return answers
 
+
+def get_sim_score(model, questions, para_chunks):
+  scores = []
+  for que in questions:
+    biencoder_ip = [[que['question'], chunk] for chunk in para_chunks]
+    score = model.predict(biencoder_ip)
+    scores.append(score)
+
+  return scores
